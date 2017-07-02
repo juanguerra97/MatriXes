4549994f8948fa4a681fa4739ea23379d5a556cf
Juan Guerra <juanguerra97@protonmail.com>
2017-07-02 17:41:07 -0600

    Implementación del método multiplicar() que realizar una multiplicacion de matrices

--- src/matrixes/Matriz.java
+++ src/matrixes/Matriz.java
@@ -30,6 +30,28 @@ public class Matriz {
 		return diferencia;
 	}
 	
+	// método para multiplicar dos matrices
+	public static double[][] multiplicar(double[][] matriz1, double[][] matriz2){
+		double[][] producto = null;
+		int filas1 = matriz1.length;// filas de la primer matriz
+		int columnas1 = matriz1[0].length;// columnas de la primer matriz
+		int filas2 = matriz2.length;// filas de la segunda matriz
+		int columnas2 = matriz2[0].length;// columnas de la segunda matriz
+		if(columnas1 == filas2){// comprueba que las dimensiones sean validas
+			producto = new double[filas1][columnas2];
+			double resultado = 0;
+			for(int f1 = 0; f1 < filas1; ++f1)// filas de la primer matriz
+				for(int c2 = 0; c2 < columnas2; ++c2){//	columnas de la segunda matriz
+					resultado = 0;
+					for(int c1 = 0; c1 < columnas1; ++c1)//	columnas de la primer matriz
+						resultado+=(matriz1[f1][c1] * matriz2[c1][c2]);
+					producto[f1][c2] = resultado;
+				}
+					
+		}
+		return producto;
+	}
+	
 	// método que multiplica una matriz por un valor escalar
 	public static double[][] multiplicar(double[][] matriz, double escalar){
 		for(int f = 0; f < matriz.length; ++f)	//	filas
