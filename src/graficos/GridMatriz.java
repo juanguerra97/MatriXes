c2f33e2a5b5e5e14b9545255b24b2e26edc66ceb
Juan Guerra <juanguerra97@protonmail.com>
2017-07-01 19:21:09 -0600

    Creacion de clase GridMatriz

---
+++ src/graficos/GridMatriz.java
@@ -0,0 +1,10 @@
+package graficos;
+
+import javax.swing.JPanel;
+
+//	Clase que muestra un interfaz donde se puede ingresar/mostrar los valores de una matriz
+public class GridMatriz extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+}
