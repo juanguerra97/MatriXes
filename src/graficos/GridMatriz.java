a3344b01dc0d0e8de1bef944395d57c7a119485f
Juan Guerra <juanguerra97@protonmail.com>
2017-07-01 19:31:04 -0600

    Se añadió la funcion setDimensiones()

--- src/graficos/GridMatriz.java
+++ src/graficos/GridMatriz.java
@@ -17,14 +17,25 @@ public class GridMatriz extends JPanel {
 	
 	// constructor
 	public GridMatriz(int filas, int columnas){
-		this.filas = filas;
-		this.columnas = columnas;
 		fieldsMatriz = new JTextField[4][4];// inicialización del arreglo(una matriz puede ser máximo de 4x4)
 		for(int f = 0; f < 4; ++f)	//	filas
 			for(int c = 0; c < 4; ++c){	//	columnas
 				fieldsMatriz[f][c] = new JTextField(8);// inicialización del campo de texto
 				fieldsMatriz[f][c].setHorizontalAlignment(SwingConstants.RIGHT);// alinea el texto de los campos a la derecha
 			}
+		setDimensiones(filas, columnas);// llamada a la funcion que establece las dimensiones
 	}// termina el constructor
+	
+	// método para establecer las dimensiones de la matriz que se esta mostrando/ingresando
+	public void setDimensiones(int filas, int columnas){
+		if(filas > 0 && filas <= 4)
+			this.filas = filas;
+		else
+			this.filas = 3;
+		if(columnas > 0 && columnas <= 4)
+			this.columnas = columnas;
+		else
+			this.columnas = 3;
+	}
 	
 }
