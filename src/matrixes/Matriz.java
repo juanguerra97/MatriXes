ef24f4b47038ae4a30eb5ee6b5dd46ac03503d01
Juan Guerra <juanguerra97@protonmail.com>
2017-07-02 17:06:41 -0600

    Implementacion de la funcion restar() que resta dos matrices

--- src/matrixes/Matriz.java
+++ src/matrixes/Matriz.java
@@ -16,4 +16,18 @@ public class Matriz {
 		return suma;
 	}
 
+	// método que resta dos matrices
+	public static double[][] restar(double[][] matriz1, double[][] matriz2){
+		double[][] diferencia = null;
+		int filas = matriz1.length;
+		int columnas = matriz1[0].length;
+		if(filas == matriz2.length && columnas == matriz2[0].length){// se comprueba que las dos matrices tengan las mismas dimensiones de lo contrario se devuelve null
+			diferencia = new double[filas][columnas];// inicializa el arreglo que guarda el resultado
+			for(int f = 0; f < filas; ++f)	//	filas
+				for(int c = 0; c < columnas; ++c)	//	columnas
+					diferencia[f][c] = matriz1[f][c] - matriz2[f][c];
+		}
+		return diferencia;
+	}
+		
 }
