40b278f74bebd6f84678e66e62debaaf2f3cea4b
Juan Guerra <juanguerra97@protonmail.com>
2017-07-02 12:12:58 -0600

    Interfaz de la operacion

---
+++ src/graficos/InterfazResta.java
@@ -0,0 +1,21 @@
+package graficos;
+
+public class InterfazResta extends BaseInterfazSumaResta {
+
+	private static final long serialVersionUID = 1L;
+
+	public InterfazResta() {
+		super("Restar");
+		
+	}
+
+	@Override
+	public void hacerOperacion() {
+
+		double[][] matriz1 = gridMatriz1.getMatriz();
+		double[][] matriz2 = gridMatriz2.getMatriz();
+		double[][] diferencia;
+		
+	}
+
+}
---
+++ src/graficos/InterfazSuma.java
@@ -0,0 +1,20 @@
+package graficos;
+
+public class InterfazSuma extends BaseInterfazSumaResta {
+
+	private static final long serialVersionUID = 1L;
+
+	public InterfazSuma() {
+		super("Sumar");		
+	}
+
+	@Override
+	public void hacerOperacion() {
+		
+		double[][] matriz1 = gridMatriz1.getMatriz();
+		double[][] matriz2 = gridMatriz2.getMatriz();
+		double[][] suma;
+		
+	}
+
+}
