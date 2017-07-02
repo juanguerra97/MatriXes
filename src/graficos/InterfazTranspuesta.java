e183d9a89693d8240deb1803cb77cee321f3d27d
Juan Guerra <juanguerra97@protonmail.com>
2017-07-02 12:33:20 -0600

    Interfaz para la operación de obtener la transpuesta de una matriz

---
+++ src/graficos/InterfazTranspuesta.java
@@ -0,0 +1,16 @@
+package graficos;
+
+public class InterfazTranspuesta extends BaseInterfazOperacionUnaMatriz {
+
+	private static final long serialVersionUID = 1L;
+
+	public InterfazTranspuesta() {
+		super("Transponer");
+	}
+
+	@Override
+	public void hacerOperacion() {
+		
+	}
+
+}
