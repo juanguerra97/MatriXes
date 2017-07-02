01614179b1edc794b1977983c7faf6857f2bc1c8
Juan Guerra <juanguerra97@protonmail.com>
2017-07-02 17:16:37 -0600

    Termina de implementarse el método hacerOperacion() en la interfaz de suma

--- src/graficos/InterfazSuma.java
+++ src/graficos/InterfazSuma.java
@@ -1,5 +1,7 @@
 package graficos;
 
+import matrixes.Matriz;
+
 public class InterfazSuma extends BaseInterfazSumaResta {
 
 	private static final long serialVersionUID = 1L;
@@ -13,7 +15,9 @@ public class InterfazSuma extends BaseInterfazSumaResta {
 		
 		double[][] matriz1 = gridMatriz1.getMatriz();
 		double[][] matriz2 = gridMatriz2.getMatriz();
-		double[][] suma;
+		double[][] suma = Matriz.sumar(matriz1, matriz2);
+		gridMatrizResultado.setDimensiones(suma.length, suma[0].length);
+		gridMatrizResultado.setMatriz(suma);
 		
 	}
 
